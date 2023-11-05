0d6b38ee74b78f4bc24ce9b803032201417f0fc6
nikita.peretolchin <nikita.peretolchin@gmail.com>
2023-11-05 19:45:30 +0700

    init chat

--- front/app/layout.js
+++ front/app/layout.js
@@ -11,32 +11,23 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import DashboardIcon from "@mui/icons-material/Dashboard";
+import ChatIcon from "@mui/icons-material/Chat";
 import HomeIcon from "@mui/icons-material/Home";
-import StarIcon from "@mui/icons-material/Star";
-import ChecklistIcon from "@mui/icons-material/Checklist";
 import SettingsIcon from "@mui/icons-material/Settings";
-import SupportIcon from "@mui/icons-material/Support";
 import LogoutIcon from "@mui/icons-material/Logout";
 import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
 
 export const metadata = {
-  title: "Next.js App Router + Material UI v5",
-  description: "Next.js App Router + Material UI v5",
+  title: "My chat",
+  description: "The chat made by Next.js + Material UI",
 };
 
 const DRAWER_WIDTH = 240;
 
 const LINKS = [
   { text: "Home", href: "/", icon: HomeIcon },
-  { text: "Starred", href: "/starred", icon: StarIcon },
-  { text: "Tasks", href: "/tasks", icon: ChecklistIcon },
-];
-
-const PLACEHOLDER_LINKS = [
-  { text: "Settings", icon: SettingsIcon },
-  { text: "Support", icon: SupportIcon },
-  { text: "Logout", icon: LogoutIcon },
+  { text: "Settings", href: "/settings", icon: SettingsIcon },
+  { text: "Logout", href: "/logout", icon: LogoutIcon },
 ];
 
 export default function RootLayout({ children }) {
@@ -44,16 +35,20 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <ThemeRegistry>
-          <AppBar position="fixed" sx={{ zIndex: 2000 }}>
-            <Toolbar sx={{ backgroundColor: "background.paper" }}>
-              <DashboardIcon
-                sx={{ color: "#444", mr: 2, transform: "translateY(-2px)" }}
-              />
-              <Typography variant="h6" noWrap component="div" color="black">
-                Next.js App Router
-              </Typography>
-            </Toolbar>
-          </AppBar>
+          {/*<AppBar position="fixed" sx={{ zIndex: 2000 }}>*/}
+          {/*  <Toolbar sx={{ backgroundColor: "background.paper" }}>*/}
+          {/*    <ChatIcon*/}
+          {/*      sx={{*/}
+          {/*        color: "#444",*/}
+          {/*        mr: 2,*/}
+          {/*        transform: "translateY(-2px)",*/}
+          {/*      }}*/}
+          {/*    />*/}
+          {/*    <Typography variant="h6" noWrap component="div" color="black">*/}
+          {/*      My chat*/}
+          {/*    </Typography>*/}
+          {/*  </Toolbar>*/}
+          {/*</AppBar>*/}
           <Drawer
             sx={{
               width: DRAWER_WIDTH,
@@ -61,7 +56,7 @@ export default function RootLayout({ children }) {
               "& .MuiDrawer-paper": {
                 width: DRAWER_WIDTH,
                 boxSizing: "border-box",
-                top: ["48px", "56px", "64px"],
+                // top: ["48px", "56px", "64px"],
                 height: "auto",
                 bottom: 0,
               },
@@ -69,24 +64,11 @@ export default function RootLayout({ children }) {
             variant="permanent"
             anchor="left"
           >
-            <Divider />
+            <Divider sx={{ mt: "auto" }} />
             <List>
               {LINKS.map(({ text, href, icon: Icon }) => (
                 <ListItem key={href} disablePadding>
                   <ListItemButton component={Link} href={href}>
-                    <ListItemIcon>
-                      <Icon />
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-            <Divider sx={{ mt: "auto" }} />
-            <List>
-              {PLACEHOLDER_LINKS.map(({ text, icon: Icon }) => (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton>
                     <ListItemIcon>
                       <Icon />
                     </ListItemIcon>
@@ -102,8 +84,8 @@ export default function RootLayout({ children }) {
               flexGrow: 1,
               bgcolor: "background.default",
               ml: `${DRAWER_WIDTH}px`,
-              mt: ["48px", "56px", "64px"],
-              p: 3,
+              // mt: ["48px", "56px", "64px"],
+              // p: 3,
             }}
           >
             {children}
